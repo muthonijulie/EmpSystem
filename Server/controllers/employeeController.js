e85fc2db75e93e79551995e80167e835b98098d4
Evans Mungai <thetracker254@gmail.com>
2024-05-07 22:06:40 +0300

    Set up employee details and assigned tasks function in employee controller

--- Server/controllers/employeeController.js
+++ Server/controllers/employeeController.js
@@ -1,5 +1,41 @@
 const db = require('../config/database');
 
-exports.homepage = (req, res)=>{
-    res.render('./layouts/employeeHomepage');
+exports.homepage = async (req, res) => {
+    db.query('select * from empdetails', (err, result) => {
+        if (err) {
+            console.log(err)
+        } else {
+            if (result.length === 0) {
+                res.send("No records found");
+            } else {
+                res.render('./layouts/employeeHomepage', {
+                    sampleData: result
+                });
+            }
+        }
+    })
+}
+exports.showAssignedTasks = async (req, res) => {
+    var employeeId = {
+        empId: req.body.emp_id
+    }
+    db.query('select * from tasks where emp_id=?', employeeId, (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            if (result.length === 0) {
+                res.send("No tasks assigned yet")
+            } else {
+                res.render('./layouts/employeeAssignedTasks', {
+                    sampleData: result
+                })
+            }
+        }
+    })
+}
+exports.overdraftApplication = async (req, res) => {
+
+}
+exports.leaveApplication = async (req, res) => {
+
 }
--- Server/routes/employeeDashboard.js
+++ Server/routes/employeeDashboard.js
@@ -2,4 +2,5 @@ const  employeeController= require('../controllers/employeeController');
 
 module.exports = (app) => {
     app.get('/employee', employeeController.homepage);
+    app.get('/employee/assignedtasks', employeeController.showAssignedTasks);
 }
