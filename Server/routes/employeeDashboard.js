afead42ee59d984d9920e6cf15f1816733dd651e
Evans Mungai <thetracker254@gmail.com>
2024-05-08 03:14:13 +0300

    Finish Setting up Employee controller functions

--- Server/routes/employeeDashboard.js
+++ Server/routes/employeeDashboard.js
@@ -3,4 +3,10 @@ const  employeeController= require('../controllers/employeeController');
 module.exports = (app) => {
     app.get('/employee', employeeController.homepage);
     app.get('/employee/assignedtasks', employeeController.showAssignedTasks);
+    app.get('/application/overdraft', employeeController.overdraftForm);
+    app.post('/application/overdraft', employeeController.overdraftApplication);
+    app.get('/application/overdraftdetails', employeeController.overdraftDetails);
+    app.get('/application/leave', employeeController.leaveForm)
+    app.post('/application/leave', employeeController.leaveApplication);
+    app.get('/application/leavedetails', employeeController.leaveDetails);
 }
